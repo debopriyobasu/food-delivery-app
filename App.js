881a1337266c7509e52dfa36cd7989a53f4341e6
Debopriyo Basu <debopriyobasu@Debopriyos-MacBook-Air.local>
2023-11-07 14:05:27 +0530

    added npm files, gitignore and parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from'react'
+import ReactDOM from 'react-dom/client'
+
 const header = React.createElement("h1", {id:"header"}, "Hello World from React!");
 console.log(header)
 const root = ReactDOM.createRoot(document.getElementById("root2"));

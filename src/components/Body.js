ca793ba6df0186c97133a49fd95bf879d9ca1574
Debopriyo Basu <debopriyobasu@gmail.com>
2023-12-03 21:52:19 +0530

    implemented search

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,13 +3,15 @@ import { useEffect, useState } from "react";
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
   const [originalRestaurants, setOriginalListOfRestaurants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const [filtered, setFiltered] = useState(false);
   useEffect(() => {
     fetchData();
   }, []);
+
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D12.9715987%26lng%3D77.5945627%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
@@ -25,22 +27,48 @@ const Body = () => {
     (res) => res.info.avgRating > 4.2
   );
 
+  const searchFilteredList = () => {
+    const searchFilter = originalRestaurants.filter((res) =>
+      res.info.name
+        .toLowerCase()
+        .includes(searchTerm === "" ? "" : searchTerm.toLowerCase())
+    );
+    setListOfRestaurants(searchFilter);
+  };
+
   if (listOfRestaurants.length === 0) return <h1>Loading...</h1>;
   return (
     <div className="md:mx-auto max-w-screen-lg my-2">
-      <button
-        className={`${
-          filtered === false ? "bg-gray-300" : "bg-gray-500"
-        } hover:bg-gray-500 text-white font-bold py-2 px-2 rounded`}
-        onClick={() => {
-          filtered === false
-            ? (setFiltered(true), setListOfRestaurants(filteredList))
-            : (setFiltered(false), setListOfRestaurants(originalRestaurants));
-        }}
-      >
-        Filter
-      </button>
-      <section className="grid grid-cols-5 gap-5 py-5">
+      <div className="flex justify-between">
+        <div className="flex gap-2">
+          <input
+            id="searchField"
+            type="text"
+            className=""
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+          <button className="" onClick={searchFilteredList}>
+            Search
+          </button>
+        </div>
+
+        <button
+          className={`${
+            filtered === false ? "bg-gray-300" : "bg-gray-500"
+          } hover:bg-gray-500 text-white font-bold py-1 px-1 rounded`}
+          onClick={() => {
+            filtered === false
+              ? (setFiltered(true), setListOfRestaurants(filteredList))
+              : (setFiltered(false), setListOfRestaurants(originalRestaurants));
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
+
+      <section className="grid grid-cols-4 gap-4 py-4">
         {listOfRestaurants.map((restaurant) => (
           <Card key={restaurant.info.id} restaurantData={restaurant} />
         ))}
